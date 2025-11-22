056aa8274fe8d30358251de0fa9005b895aea361
Brindha <brindha90@hotmail.co.uk>
2025-11-22 13:16:17 +0000

    Update project files and remove old tests

--- src/main/java/com/example/brindhaspetitions/BrindhaspetitionsApplication.java
+++ src/main/java/com/example/brindhaspetitions/BrindhaspetitionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrindhaspetitionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BrindhaspetitionsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BrindhaspetitionsApplication.class, args);
+    }
 }
